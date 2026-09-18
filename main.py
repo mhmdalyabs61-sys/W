d342fc23beff82af176c1633cbd2b86e063037ea
mhmdalyabs61-sys <mhmdalyabs61@gmail.com>
2026-09-18 17:30:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True  # <--- هذا السطر الأساسي والمهم جداً
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="", intents=intents)
 
 
 
